4e2f72a29ba2b5e5a094af8beb22df5eac371a57
ErinSolstice <pherke1@lsu.edu>
2021-11-17 02:00:39 -0600

    finished adding in joint friction initial formulation

--- frictionIDP.m
+++ frictionIDP.m
@@ -2,12 +2,14 @@
 % Bailey Smoorenburg, Connor McCarthy, Gavin Sheng, Jill Bohnet, Patrick Herke
 
 w2 = 1;
-P4 = 0;
+P4 = 10;
 
 %function ax = frictionIDP(w2, P4)
     tol = 1e-8;
     mu = 0.2;
     R = 0.2; % in
+    rw = 1.7/2 % in % half the width of the slider
+    rh = 1.0/2 % in % half the height of the slider
     
     kinData = readtable('kinematicData.xlsx');
 
@@ -55,6 +57,7 @@ P4 = 0;
 
     Rcg3 = 3.347; %inches, pythag thm from pic
 
+    % initializing table to store values
     sz = [361 13];
     varNames = ["F12x","F12y","F23","F23x","F23y","F13","F13x","F13y","F34x","F34y","F14","R7F14","T2"];
     varTypes = repmat("double",1,13);
@@ -77,11 +80,13 @@ P4 = 0;
         a_g4y = 0;
         a_g4x = (f4(i)*alpha2 + f4p(i)*w2^2)/12; % fp_g4x = fp4
         
-        fric = zeros(4,1);
+        fric = ones(5,1);
         F12 = 0;
         F34 = 0;
         F23n = 0;
         F13n = 0;
+        F14 = 0;
+        R7F14 = 0;
         
         % input directional indicator
         Din = sign(w2);
@@ -104,21 +109,9 @@ P4 = 0;
         end
         
         % slider in slot directional indicator
-        D14 = 0;
+        D14 = sign(-f4(i))*Din;
         
         for j = 1:100
-            % A = 9x9
-            A = [1, 0, -cos(th3(i)-(3*pi/2)),                0,           0,    0, 0, 0, 0;
-                 0, 1, -sin(th3(i)-(3*pi/2)),                0,           0,    0, 0, 0, 0;
-                 0, 0, cos(th3(i)-(3*pi/2)),     cos(th3(i)-(3*pi/2)), -1,   0, 0, 0, 0;
-                 0, 0, sin(th3(i)-(3*pi/2)),     sin(th3(i)-(3*pi/2)), 0,   -1, 0, 0, 0;
-                 0, 0,          0,                              0,           1,    0, 0, 0, 0;
-                 0, 0,          0,                              0,           0,    1, 1, 0, 0;
-                 0, 0, -R2*cos(th3(i)-th2(i)),     0,           0,    0, 0, 0, 1;
-                 0, 0,         R3(i),                      R3(i)-R5(i),  0,    0, 0, 0, 0;
-                 0, 0,          0,                              0,           0,    0, 0, 1, 0];
-
-
             % T12 = mu*R*|F12|*D12 acting on link 1
             T12 = mu*R*F12*D12;
             % T34 = mu*R*|F34|*D34 acting on link 4
@@ -135,24 +128,65 @@ P4 = 0;
             f23 = mu*F23n;
             f23x = f23*cos(th3(i) - pi);
             f23y = f23*sin(th3(i) - pi);
+            % f14 acting on link 4
+            N14 = [F14; R7F14]\[1 1; rw -rw];
+            f14 = mu*(abs(N14(1)) + abs(N14(2)))*D14;
+            % the book swaps the signs on T14 depending on >0 but I don;t
+            % think that's necessary
+            % it also is used if height above and below the block C pin
+            % aren't the same
+            if N14(1) > 0
+                T14(1) = mu*rh*N14(1)*D14;
+            else
+                T14(1) = mu*rh*N14(2)*D14;
+            end
             
+            if N14(2) > 0
+                T14(2) = mu*rh*N14(2)*D14;
+            else
+                T14(2) = mu*rh*N14(2)*D14;
+            end
+            
+            % storing the old friction values
+            fricOld = fric;
+            fric = [(1/sqrt(1/mu^2 + 1))*F12;
+                    (1/sqrt(1/mu^2 + 1))*F34;
+                    f13;
+                    f23;
+                    f14;]
+           
+            relError = norm(fric - fricOld)/norm(fricOld);
+            if relError < tol
+                break
+            end
+            
+            % A = 9x9
+            A = [1, 0, -cos(th3(i)-(3*pi/2)),                0,           0,    0, 0, 0, 0;
+                 0, 1, -sin(th3(i)-(3*pi/2)),                0,           0,    0, 0, 0, 0;
+                 0, 0, cos(th3(i)-(3*pi/2)),     cos(th3(i)-(3*pi/2)), -1,   0, 0, 0, 0;
+                 0, 0, sin(th3(i)-(3*pi/2)),     sin(th3(i)-(3*pi/2)), 0,   -1, 0, 0, 0;
+                 0, 0,          0,                              0,           1,    0, 0, 0, 0;
+                 0, 0,          0,                              0,           0,    1, 1, 0, 0;
+                 0, 0, -R2*cos(th3(i)-th2(i)),     0,           0,    0, 0, 0, 1;
+                 0, 0,         R3(i),                      R3(i)-R5(i),  0,    0, 0, 0, 0;
+                 0, 0,          0,                              0,           0,    0, 0, 1, 0];
             
             jKinem = [m2*a_g2x;
-                    m2*a_g2y + m2*g; %center of gravity is at pin + m2*g;
+                    m2*a_g2y;
                     m3*a_g3x;
-                    m3*a_g3y + m3*g;
-                    m4*a_g4x - P4;
-                    m4*a_g4y + m4*g;
+                    m3*a_g3y;
+                    m4*a_g4x;
+                    m4*a_g4y;
                     Ig_2*alpha2/12;
                     Ig_3*alpha3/12 + m3*Rcg3*(cos(th3(i))*a_g3y - sin(th3(i))*a_g3x + m3*g*cos(th3(i)));
                     0];
             
             jForce = [f23x;
-                    f23y;
+                    -m2*g + f23y;
                     -f13x + -f23x;
-                    -f13y + -f23y;
-                    0;
-                    0;
+                    -m3*g + -f13y + -f23y;
+                    P4 + f14;
+                    -m4*g;
                     -T12 + T23;
                     -T34 + -T13 + -T23;
                     T34];
@@ -165,24 +199,14 @@ P4 = 0;
             F34 = sqrt(x(5)^2 + x(6)^2); % F34 = sqrt(F34x^2 + F34y^2)
             F13n = x(4);
             F23n = x(3);
-            
-            fricOld = fric;
-            fric = [(1/sqrt(1/mu^2 + 1))*F12;
-                    (1/sqrt(1/mu^2 + 1))*F34;
-                    mu*F13n;
-                    mu*F23n;]
+            F14 = x(7);
+            R7F14 = x(8);
 
-            
 %             fricOld = fric;
 %             fric = (1/sqrt(1/mu^2 + 1)).*[F12; F34; F13; F23; F14];
 
             % x = [  1,    2,    3,    4,   5,    6,    7,    8,    9]
             % x = [F12x, F12y, F23n, F13n, F34x, F34y, F14, R7F14, T2]
-            
-            relError = norm(fric - fricOld)/norm(fricOld);
-            if relError < tol
-                break
-            end
         end
         disp(j)
 
@@ -229,7 +253,8 @@ P4 = 0;
 
     % finding indices of the local minimums and maximums for the position graph
     for i=1:8
-        posMinMax.(posColNames{i}) = [find(forcesIDP.(posColNames{i}) == min(forcesIDP.(posColNames{i}))); find(forcesIDP.(posColNames{i}) == max(forcesIDP.(posColNames{i})))];
+        maxes = (find(forcesIDP.(posColNames{i}) == max(forcesIDP.(posColNames{i}))));
+        posMinMax.(posColNames{i}) = [find(forcesIDP.(posColNames{i}) == min(forcesIDP.(posColNames{i}))); maxes(1)];
     end
 
     % defining the figure
